56f22a1443dcdcf246f20217663aad646938e9f2
Nick Tkachov <ntkachov@gmail.com>
2011-12-31 22:25:05 -0500

    Removed Calendar

--- BlogServ/calGen.js
+++ BlogServ/calGen.js
@@ -4,8 +4,8 @@ var amazon = require('../BackEnd/amazon.js');
 var PC = require('../TagEngine/PostCollector.js');
 exports.cal = "";
 
-var calText = ' <div class="sideblock"> <h4 class="events">Reminders:</h4></div><div class="CalWidget sideblock"><div class="CalBar"><p class="headline"> #&$1 </p><p class="time">#&$1t</p></div><div class="MidBar"><p class="headline">#&$2</p><p class="time">#&$2t</p></div><div class="MidBar"> <p class="headline">#&$3</p><p class="time">#&$3t</p></div><div class="BotBar"><p class="headline">#&$4</p><p class="time">#&$4t</p></div></div>';
-exports.loadCal = function(){
+var calText = ""// ' <div class="sideblock"> <h4 class="events">Reminders:</h4></div><div class="CalWidget sideblock"><div class="CalBar"><p class="headline"> #&$1 </p><p class="time">#&$1t</p></div><div class="MidBar"><p class="headline">#&$2</p><p class="time">#&$2t</p></div><div class="MidBar"> <p class="headline">#&$3</p><p class="time">#&$3t</p></div><div class="BotBar"><p class="headline">#&$4</p><p class="time">#&$4t</p></div></div>';
+exports.loadCal = function(){/*
 amazon.loadFile("/cal.txt", function(path, contentType, thisData, responce){
 
 	var days = thisData.split('|');
@@ -20,5 +20,5 @@ amazon.loadFile("/cal.txt", function(path, contentType, thisData, responce){
 	}
 	console.log(exports.cal);
 
-},"");
+},"");*/
 }
